94d9289ac6c6641972fd333b54566a7d829e152c
St3fan097 <Ste97.esse@gmail.com>
2023-12-28 19:02:27 +0100

    es-2

---
+++ esercizi/es-2/es_2.js
@@ -0,0 +1,6 @@
+/* const crypto = require('crypto');
+Object.keys(crypto);
+const randomId = crypto.randomBytes(16).toString('hex');
+console.log(randomId); */
+
+//ho trovato questo metodo e su REPL ha funzionato
