b3027938c983bbaf47facdd51c5381ca1159552c
St3fan097 <Ste97.esse@gmail.com>
2023-12-28 16:09:27 +0100

    es-1

---
+++ esercizi/es-1/es_1.js
@@ -0,0 +1,16 @@
+const a = 5;
+const b = 6;
+
+console.log(a + b);
+
+function sommaNormale(a,b){
+    return a + b;
+};
+
+console.log(sommaNormale(5,6));
+
+const sommaArrow = (a,b) => {
+    return a + b;
+};
+
+console.log(sommaArrow(5,6));
