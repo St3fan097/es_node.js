f013992258472e597c2cec8f2cb4603d4becebcb
St3fan097 <Ste97.esse@gmail.com>
2024-01-03 16:23:30 +0100

    es-10

---
+++ esercizi/es-10/exercise_10.mjs
@@ -0,0 +1,29 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+        const win = Boolean(Math.round(Math.random()));
+
+        process.nextTick(() => {
+            if (win) {
+                resolve(`${player} won a prize in the draw!`);
+            } else {
+                reject(new Error(`${player} lost the draw.`));
+            }
+        });
+    });
+}
+
+async function agetResults(){
+    try {
+        const player1 = await luckyDraw("Tina");
+        console.log(player1);
+        const player2 = await luckyDraw("Jorge");
+        console.log(player2);
+        const player3 = await luckyDraw("Julien");
+        console.log(player3);;
+        
+    } catch (error) {
+       console.error(error); 
+    }
+} 
+
+agetResults();
