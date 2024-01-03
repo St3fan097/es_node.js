b9a3639141453fc425ebad49e513afad5cf38491
St3fan097 <Ste97.esse@gmail.com>
2024-01-03 16:44:14 +0100

    es-11

---
+++ esercizi/es-11/exercise_11.mjs
@@ -0,0 +1,32 @@
+import { EventEmitter }  from "node:events";
+
+function createNewsFeed() {
+    const emitter = new EventEmitter();
+
+    setInterval(() => {
+        emitter.emit("newsEvent", "News: A thing happened in a place.");
+    }, 1000);
+
+    setInterval(() => {
+        emitter.emit("breakingNews", "Breaking news! A BIG thing happened.");
+    }, 4000);
+
+    setTimeout(() => {
+        emitter.emit("error", new Error("News feed connection error"));
+    }, 5000);
+
+    return emitter;
+}
+
+const newsFeed = createNewsFeed();
+newsFeed.on("newsEvent", (message) => {
+    console.log(`Received news: ${message}`);
+});
+
+newsFeed.on("breakingNews", (message) => {
+    console.log(`Breaking news: ${message}`);
+});
+
+newsFeed.on("error", (error) => {
+    console.error(`Error: ${error.message}`);
+});
