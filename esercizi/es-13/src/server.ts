19ac5f79780c3b8b04f1a55473e7fdf2eb52d2ed
St3fan097 <Ste97.esse@gmail.com>
2024-01-20 17:18:26 +0100

    exercise-13

---
+++ esercizi/es-13/src/server.ts
@@ -0,0 +1,31 @@
+import  express  from "express";
+import "express-async-errors";
+import  morgan  from "morgan";
+import { getAll, getById, create, updateById, deleteById } from "./planets.js"
+//mi riferisco al server express
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+//utilizzo un metodo morgan per fare la chiamata al server
+
+app.use(morgan("dev"));
+
+
+//metodo
+app.get("/api/planets", getAll);
+
+app.get("/api/planets/:id", getById);
+
+app.post("/api/planets", create);
+
+app.put("/api/planets/:id", updateById);
+
+app.delete("/api/planets/:id", deleteById)
+
+//avvio server
+app.listen(port, () => {
+    console.log(`Listening to port http://localhost:${port}`);
+});
+
